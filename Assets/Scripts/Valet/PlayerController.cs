1b92b0eff8ab20351f03082404ef059757684d0e
ValetNoir <88502824+ValetNoir@users.noreply.github.com>
2025-01-25 10:52:33 +0100

    Finished Character controller (+added dash)

--- Assets/Scripts/Valet/PlayerController.cs
+++ Assets/Scripts/Valet/PlayerController.cs
@@ -7,10 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 10f;
+    public float dashForce = 10f;
+    public float dashCooldown = 5f;
     public LayerMask groundLayer;
+
+    public bool facingRight = true;
 
     private Rigidbody2D rb;
     private Collider2D collider2d;
+
+    private float dashTimer = 0f;
 
     private void Awake()
     {
@@ -20,31 +26,48 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        dashTimer = Math.Max(dashTimer - Time.deltaTime, 0f);
+
         bool isGrounded = Physics2D.IsTouchingLayers(collider2d, groundLayer);
 
         float horizontalInput = Input.GetAxis("Horizontal");
-        
-        transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
+
+        if(horizontalInput >= 0.1f)
+            rb.linearVelocityX = Math.Max(rb.linearVelocityX, horizontalInput * speed);
+        else if(horizontalInput <= -0.1f)
+            rb.linearVelocityX = Math.Min(rb.linearVelocityX, horizontalInput * speed);
+
 
         FlipCharacter(horizontalInput);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
+        if(Input.GetButtonDown("Jump") && isGrounded) {
             rb.linearVelocityY += jumpForce;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && dashTimer == 0f) {
+            dashTimer = dashCooldown;
+            if(facingRight)
+                rb.linearVelocityX += dashForce;
+            else
+                rb.linearVelocityX -= dashForce;
         }
     }
 
     private void FlipCharacter(float horizontalInput)
     {
         // Flip sprite based on movement direction
-        if (horizontalInput > 0)
-            Flip(true);
-        else if (horizontalInput < 0)
-            Flip(false);
+        if(horizontalInput > 0) {
+            facingRight = true;
+            Flip();
+        }
+        else if(horizontalInput < 0) {
+            facingRight = false;
+            Flip();
+        }
 
     }
 
-    void Flip(bool facingRight)
+    void Flip()
     {
         // Flip the sprite horizontally
         Vector3 scale = transform.localScale;
