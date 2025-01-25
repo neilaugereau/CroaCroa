c02ad25495997a6437b12fcfba2faa7b2f65a183
Neil Augereau <neil.augereau79@gmail.com>
2025-01-25 12:31:25 +0100

    feat : Added Bubbles (Manager, Settings and Functions)

---
+++ Assets/Scripts/Bubbles/BubbleShooter.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BubbleShooter : MonoBehaviour
+{
+    public PlayerController playerController;
+    public Transform shootPoint;
+    public BubbleType bubbleType;
+    
+    private bool isShooting = false;
+    void Update()
+    {
+        // Détecter si le joueur appuie sur la touche pour tirer
+        if (Input.GetKeyDown(KeyCode.F) && !isShooting)
+        {
+            StartCoroutine(ShootBurst());
+        }
+    }
+
+    private IEnumerator ShootBurst()
+    {
+        isShooting = true;
+
+        for (int i = 0; i < bubbleType.burstCount; i++)
+        {
+            // Créer une bulle au point de tir
+            GameObject bubble = Instantiate(bubbleType.prefab, shootPoint.position, Quaternion.identity);
+            
+            float randomSize = Random.Range(bubbleType.sizeThreshold.x, bubbleType.sizeThreshold.y);
+            bubble.transform.localScale = bubble.transform.localScale * randomSize;
+            
+            // Ajouter une force pour lancer la bulle
+            Rigidbody2D rb = bubble.GetComponent<Rigidbody2D>();
+            Vector2 randomDir = new Vector2(0 ,Random.Range(bubbleType.dispersionThreshold.x, bubbleType.dispersionThreshold.y) );
+            if(playerController.facingRight)
+                rb.AddForce((Vector2.right +randomDir) * bubbleType.shootForce, ForceMode2D.Impulse);
+            else
+                rb.AddForce((Vector2.left +randomDir) * bubbleType.shootForce, ForceMode2D.Impulse);
+            
+            // Attendre avant de tirer la prochaine bulle
+            yield return new WaitForSeconds(bubbleType.fireRate);
+        }
+
+        isShooting = false;
+    }
+}
---
+++ Assets/Scripts/Bubbles/BubbleType.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BubbleType", menuName = "Bubbles/BubbleType")]
+public class BubbleType : ScriptableObject
+{
+    public GameObject prefab;
+    public Vector2 sizeThreshold;
+    public Vector2 gravityThreshold;
+    public Vector2 dispersionThreshold;
+    public float shootForce = 10f; 
+    public float fireRate = 0.2f;  
+    public int burstCount = 10;
+}
