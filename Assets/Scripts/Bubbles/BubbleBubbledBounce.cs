179024167f7456d6613ce49711308181eccc0160
ValetNoir <88502824+ValetNoir@users.noreply.github.com>
2025-01-25 21:30:59 +0100

    main scene update : spawner + gamemanager

--- Assets/Scripts/Bubbles/BubbleBubbledBounce.cs
+++ Assets/Scripts/Bubbles/BubbleBubbledBounce.cs
@@ -9,7 +9,7 @@ public class BubbleBubbledBounce : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
-            if (playerRb != null)
+            if (playerRb != null && collision.attachedRigidbody.linearVelocityY < 0f)
             {
                 playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, bounceForce);
                 GetComponentInParent<PlayerDefeat>().Defeat();
